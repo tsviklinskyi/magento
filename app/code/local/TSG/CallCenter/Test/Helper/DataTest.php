a4bc4fd5d91e7037f3547cdfe56546b1b2ad5f89
tsviklinskyi <tsviklinskyi@gmail.com>
2019-04-18 10:58:01 +0300

    binary gap

--- app/code/local/TSG/CallCenter/Test/Helper/DataTest.php
+++ app/code/local/TSG/CallCenter/Test/Helper/DataTest.php
@@ -56,4 +56,57 @@ class TSG_Callcenter_Test_Helper_DataTest extends PHPUnit_Framework_TestCase
             '9' => array('195', '196')
         );
     }
+
+    public function binaryGap($str)
+    {
+        $maxBinaryGap = 0;
+        $first1 = strpos($str, '1');
+        if ($first1 !== false) {
+            $nextStr = substr($str,$first1 + 1);
+            $next1 = strpos($nextStr, '1');
+            while ($next1 !== false) {
+                if ($next1 > $maxBinaryGap) {
+                    $maxBinaryGap = $next1;
+                }
+                $nextStr = substr($nextStr,$next1 + 1);
+                $next1 = strpos($nextStr, '1');
+            }
+        }
+        return $maxBinaryGap;
+    }
+
+    public function testBinaryGap()
+    {
+        $str = '1000010001';
+        $this->assertEquals($this->checkBbinaryGap($str), $this->binaryGap($str));
+    }
+
+    public function checkBbinaryGap($str)
+    {
+        $binaryGap = 0;
+        switch ($str){
+            case '1000010001' :
+                $binaryGap = 4;
+                break;
+            case '100000001' :
+                $binaryGap = 7;
+                break;
+            case '10000000' :
+                $binaryGap = 0;
+                break;
+            case '000000' :
+                $binaryGap = 0;
+                break;
+            case '111111111' :
+                $binaryGap = 0;
+                break;
+            case '00000001' :
+                $binaryGap = 0;
+                break;
+            case '1001000001' :
+                $binaryGap = 5;
+                break;
+        }
+        return $binaryGap;
+    }
 }
